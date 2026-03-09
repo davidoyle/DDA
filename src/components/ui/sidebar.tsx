db3a8e1f3388bd114d22d79ec1769615290e954b
davidoyle <stopusingthisusername@gmail.com>
2026-03-09 13:09:02 -0700

    Refine hot-reload lint handling and relax forced HTTPS redirects

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 "use client"
 
 import * as React from "react"
@@ -606,10 +607,11 @@ function SidebarMenuSkeleton({
 }: React.ComponentProps<"div"> & {
   showIcon?: boolean
 }) {
-  // Random width between 50 to 90%.
+  const skeletonId = React.useId()
   const width = React.useMemo(() => {
-    return `${Math.floor(Math.random() * 40) + 50}%`
-  }, [])
+    const hash = Array.from(skeletonId).reduce((acc, char) => acc + char.charCodeAt(0), 0)
+    return `${(hash % 41) + 50}%`
+  }, [skeletonId])
 
   return (
     <div
